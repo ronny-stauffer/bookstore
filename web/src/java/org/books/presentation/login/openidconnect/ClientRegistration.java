c7635fd6c4d09adaeecb95430518b71ac3f60f13
Ronny Stauffer <ronny.stauffer@gmx.net>
2012-09-14 16:47:45 +0200

    Minor changes

--- web/src/java/org/books/presentation/login/openidconnect/ClientRegistration.java
+++ web/src/java/org/books/presentation/login/openidconnect/ClientRegistration.java
@@ -20,16 +20,23 @@ import javax.xml.bind.annotation.XmlRootElement;
  *
  * @author Ronny Stauffer
  */
+// JAXB Mapping required by JAX-RS JSON-to-Object Mapping
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.NONE)
 @Entity
 public class ClientRegistration /* implements Serializable */ {
     //private static final long serialVersionUID = 1L;
 
+    /**
+     * Database Primary Key in form of technical key
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * Business Key
+     */
     @NotNull
     @Size(min = 1, max = 100)
     @Column(nullable = false, length = 100, unique = true)
@@ -47,10 +54,15 @@ public class ClientRegistration /* implements Serializable */ {
     private String clientSecret;    
     
     // Non-Public API
+    // Standard Constructor required by JPA
+    // Dieser Konstruktor darf nur innerhalb des Persistence Layers verwendet werden.
+    // Er gilt als nicht-exposed und darf z.B. durch die Business Logic nicht verwendet werden!
     public ClientRegistration() {
         
     }
     
+    // Public API!
+    // Factory Method zum Create einer neuen Instanz.
     public static ClientRegistration create(String issuer) {
         if (issuer == null) {
             throw new NullPointerException("issuer must not be null!");
@@ -66,6 +78,8 @@ public class ClientRegistration /* implements Serializable */ {
     }
     
     // Non-Public API
+    // Diese Methode darf nur innerhalb des Persistence Layers verwendet werden.
+    // Sie gilt als nicht-exposed und darf z.B. durch die Business Logic nicht verwendet werden!
     public Long getId() {
         return id;
     }
